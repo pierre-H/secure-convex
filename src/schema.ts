09cb8d6ee381c75e462b46df7740cd326e4feab2
pierre <pirnay@pm.me>
2025-08-17 17:51:48 +0200

    Refactorisation de la fonction defineSecureTable dans schema.ts pour simplifier l'importation et améliorer la lisibilité du code. La fonction retourne désormais directement le résultat de defineTable sans conversion intermédiaire.

--- src/schema.ts
+++ src/schema.ts
@@ -1,4 +1,4 @@
-import { defineTable, TableDefinition } from "convex/server";
+import { defineTable } from "convex/server";
 import {
     valibotToConvex,
     type ValibotTable,
@@ -9,12 +9,13 @@ import {
 export function defineSecureTable<
     T extends ValibotTable<O>,
     O extends OnlyObjectOptions,
->(tableDefinition: T): TableDefinition {
+>(tableDefinition: T) {
     const convexShape = Object.fromEntries(
         Object.entries(tableDefinition).map(([name, schema]) => [
             name,
             valibotToConvex(schema),
         ])
-    );
-    return defineTable(convexShape as ConvexTableDefFromValibot<T, O>);
+    ) as ConvexTableDefFromValibot<T, O>;
+
+    return defineTable(convexShape);
 }
